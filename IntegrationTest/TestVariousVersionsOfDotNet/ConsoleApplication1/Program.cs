fe7002e9ab214551e9a49c97c9f4e883a1eff2b3
Chris McKee <chris.mckee@ivendi.com>
2016-09-14 16:08:40 +0100

    Bunch of basic console apps with debug asserts to check the packed nuget project works as expected.

---
+++ IntegrationTest/TestVariousVersionsOfDotNet/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DotNet4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "fancy";
+            const string passwordHash = "$2a$04$mHManSTXI9OrvFGtT3Nsz.eyyGumdhnj/oRAGINntWjZ7D/rtupx2";
+
+            Debug.Assert(BCrypt.Net.BCrypt.Verify(password, passwordHash));
+        }
+    }
+}
