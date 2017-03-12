b6fe7b7160c357eef7f90c3aba2590345350de68
Chris McKee <chris.mckee@ivendi.com>
2017-03-12 01:08:50 +0000

    xml-doc corrections

--- src/BCrypt.Net/HashInformation.cs
+++ src/BCrypt.Net/HashInformation.cs
@@ -1,8 +1,17 @@
 ﻿namespace BCrypt.Net
 {
+    /// <summary>
+    /// HashInformation : A value object that contains the results of interrogating a hash
+    /// Namely its settings (2a$10 for example); version (2a); workfactor (log rounds), and the raw hash returned
+    /// </summary>
     public sealed class HashInformation
     {
-        public HashInformation(string settings, string version, string workFactor, string rawHash)
+        /// <summary>Constructor. </summary>
+        /// <param name="settings">The message.</param>
+        /// <param name="version">The message.</param>
+        /// <param name="workFactor">The message.</param>
+        /// <param name="rawHash">The message.</param>
+        internal HashInformation(string settings, string version, string workFactor, string rawHash)
         {
             Settings = settings;
             Version = version;
@@ -10,9 +19,22 @@
             RawHash = rawHash;
         }
 
+        /// <summary>
+        /// Settings string
+        /// </summary>
         public string Settings { get; private set; }
+        /// <summary>
+        /// Hash Version
+        /// </summary>
         public string Version { get; private set; }
+        /// <summary>
+        /// log rounds used / workfactor
+        /// </summary>
         public string WorkFactor { get; private set; }
+        /// <summary>
+        /// Raw Hash
+        /// </summary>
         public string RawHash { get; private set; }
     }
+
 }
--- src/BCrypt.Net/HashInformationException.cs
+++ src/BCrypt.Net/HashInformationException.cs
@@ -7,14 +7,25 @@ namespace BCrypt.Net
     /// </summary>
     public sealed class HashInformationException : Exception
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         public HashInformationException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="HashInformationException" />.
+        /// </summary>
+        /// <param name="message"></param>
         public HashInformationException(string message) : base(message)
         {
         }
-
+        /// <summary>
+        /// Initializes a new instance of <see cref="HashInformationException" />.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         public HashInformationException(string message, Exception innerException) : base(message, innerException)
         {
         }
