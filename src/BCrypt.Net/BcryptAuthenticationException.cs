145b1302c073279ca2bca2adfc2c141e38fe81bd
Chris McKee <chris.mckee@ivendi.com>
2017-10-11 14:51:07 +0100

    int test

--- src/BCrypt.Net/BcryptAuthenticationException.cs
+++ src/BCrypt.Net/BcryptAuthenticationException.cs
@@ -2,22 +2,26 @@
 
 namespace BCrypt.Net
 {
+    /// <inheritdoc />
     /// <summary>Exception for signalling hash validation errors. </summary>
     public class BcryptAuthenticationException : Exception
     {
+        /// <inheritdoc />
         /// <summary>Default constructor. </summary>
         public BcryptAuthenticationException()
         {
         }
 
-        /// <summary>Initializes a new instance of <see cref="BcryptAuthenticationException" />.</summary>
+        /// <inheritdoc />
+        /// <summary>Initializes a new instance of <see cref="T:BCrypt.Net.BcryptAuthenticationException" />.</summary>
         /// <param name="message">The message.</param>
         public BcryptAuthenticationException(string message)
             : base(message)
         {
         }
 
-        /// <summary>Initializes a new instance of <see cref="BcryptAuthenticationException" />.</summary>
+        /// <inheritdoc />
+        /// <summary>Initializes a new instance of <see cref="T:BCrypt.Net.BcryptAuthenticationException" />.</summary>
         /// <param name="message">       The message.</param>
         /// <param name="innerException">The inner exception.</param>
         public BcryptAuthenticationException(string message, Exception innerException)
