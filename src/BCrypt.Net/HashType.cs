955f1d7c8ee83620e5ce621c4a5af5e7758f52e7
Chris McKee <chris.mckee@ivendi.com>
2020-04-14 14:18:16 +0100

    BREAKING CHANGE:  Drop legacy hash support, split keying for enhanced so nul/empty (rev a+) gets attached to the SHA hash. Should be consistent with other languages.

--- src/BCrypt.Net/HashType.cs
+++ src/BCrypt.Net/HashType.cs
@@ -10,11 +10,6 @@
         None = -1,
         SHA256 = 0,
         SHA384 = 1,
-        SHA512 = 2,
-
-        /// <summary>
-        ///     Will hash key using SHA384 but will not Base64 encode it prior to Crypt
-        /// </summary>
-        Legacy384 = 3
+        SHA512 = 2
     }
 }
