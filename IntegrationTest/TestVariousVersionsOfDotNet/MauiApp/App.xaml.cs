ffb06ef5982b5f3052e0fbfdb1e08131f4c192ee
Chris McKee <chris@chrismckee.co.uk>
2024-02-27 17:49:06 +0000

    Update Test Harnesses
    * Add MauiApp to TestVariousVersionsOfDotNet to test bcrypt in Android (debugging #168)

---
+++ IntegrationTest/TestVariousVersionsOfDotNet/MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ IntegrationTest/TestVariousVersionsOfDotNet/MauiApp/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp
+{
+    public static class MauiProgram
+    {
+        public static Microsoft.Maui.Hosting.MauiApp CreateMauiApp()
+        {
+            var builder = Microsoft.Maui.Hosting.MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
