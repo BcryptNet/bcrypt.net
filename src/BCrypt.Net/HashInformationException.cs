79a8a630c0f1b4727f7ed54b5d42aa2ff2125ccd
Chris McKee <chris@chrismckee.co.uk>
2025-11-12 17:51:17 +0000

    csproj faf, nest files

--- src/BCrypt.Net/HashInformationException.cs
+++ src/BCrypt.Net/HashInformationException.cs
@@ -1,8 +1,7 @@
 ﻿using System.Runtime.Serialization;
 
-namespace BCryptNet
-{
-    #if NETFRAMEWORK || NETSTANDARD
+namespace BCryptNet;
+#if NETFRAMEWORK || NETSTANDARD
     /// <summary>
     ///     Exception used to signal errors that occur during use of the hash information methods
     /// </summary>
@@ -39,34 +38,33 @@ namespace BCryptNet
         }
     }
 #else
+/// <summary>
+///     Exception used to signal errors that occur during use of the hash information methods
+/// </summary>
+public sealed class HashInformationException : Exception
+{
     /// <summary>
-    ///     Exception used to signal errors that occur during use of the hash information methods
+    ///     Default Constructor
     /// </summary>
-    public sealed class HashInformationException : Exception
+    public HashInformationException()
     {
-        /// <summary>
-        ///     Default Constructor
-        /// </summary>
-        public HashInformationException()
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of <see cref="HashInformationException" />.
-        /// </summary>
-        /// <param name="message"></param>
-        public HashInformationException(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of <see cref="HashInformationException" />.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="innerException"></param>
-        public HashInformationException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
     }
-#endif
+
+    /// <summary>
+    ///     Initializes a new instance of <see cref="HashInformationException" />.
+    /// </summary>
+    /// <param name="message"></param>
+    public HashInformationException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of <see cref="HashInformationException" />.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public HashInformationException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
 }
+#endif
